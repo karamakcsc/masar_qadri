cbd3685f9ad132eb3c2caf55e8f016ae84d8dea3
karamakcsc <karamakcsc@hotmail.com>
2025-08-13 13:50:14 +0300

    item rename

---
+++ masar_qadri/masar_qadri/doctype/update_item_code/update_item_code.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2025, KCSC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Update Item Code", {
+	update: function(frm) {
+        frappe.call({
+            method: "update_name",
+            callback: function(r) {
+                console.log("Teeeeeeeeeeeeez");
+            }
+        })
+	},
+});
---
+++ masar_qadri/masar_qadri/doctype/update_item_code/update_item_code.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2025, KCSC and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class UpdateItemCode(Document):
+	@frappe.whitelist()
+	def enqueue_update(
+     update_name, 
+     queue="long", 
+     timeout=3000
+                    )
+ 
+	def update_name():
+		items_sql = frappe.db.sql("""
+            SELECT name, item_code
+			FROM tabItem
+		""", as_dict=True)
+		try:
+			if items_sql:
+				for row in items_sql:
+					if "\n" in row.name or "\r" in row.name:
+						new_name = re.sub(r'[\r\n]+', '', row.name)
+						frappe.rename_doc("Item", row.name, new_name)
+					if "\n" in row.item_code or "\r" in row.item_code:
+						new_code = re.sub(r'[\r\n]+', '', row.item_code)
+						frappe.db.set_value("Item", row.name, "item_code", new_code)
+			frappe.msgprint("Updated Items Successfully")
+		except Exception as e:
+			frappe.throw(f"Error Updating Item: {e}")
