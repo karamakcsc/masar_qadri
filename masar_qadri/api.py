6aa215d13f4d23bbe825b950de654bd3ddf81346
karamakcsc <karamakcsc@hotmail.com>
2025-10-05 16:14:05 +0300

    update

--- masar_qadri/api.py
+++ masar_qadri/api.py
@@ -11,10 +11,26 @@ def get_user_pos_profiles():
         pluck="parent"
     )
 
-    # Optional: also include a direct user.pos_profile field if you use it
-    if not profiles and frappe.db.has_column("User", "pos_profile"):
-        profile = frappe.db.get_value("User", user, "pos_profile")
-        if profile:
-            profiles = [profile]
-
     return profiles
+
+# @frappe.whitelist()    ## Execute daily in hooks
+# def management_notification(): 
+#     management = frappe.db.sql("""
+#                         SELECT so1.company , so1.customer ,so1.customer_name , so1.transaction_date 
+#                         FROM `tabSales Order` so1
+#                         LEFT JOIN `tabSales Order` so2 
+#                             ON so1.customer = so2.customer 
+#                             AND so2.delivery_date > so1.delivery_date
+#                         WHERE DATEDIFF(CURRENT_DATE() , so1.transaction_date) = 180 and so2.name IS NULL
+#                         GROUP BY so1.customer
+#                         """ , as_dict = 1 )
+#     for m in management: 
+#         management_email = frappe.db.get_value('Customer' , m.customer , 'custom_email')
+#         if management_email is not None:
+#             frappe.sendmail(
+#                 recipients=management_email,
+#                 subject='Last Order Notification',
+#                 message=f"Dear {m.customer_name},<br>This is a notification that you have not placed any orders for the last 180 days.<br> Best regards,<br><b>{m.company}</b>",
+#                 reference_doctype='Customer',
+#                 reference_name=m.customer
+#             )
