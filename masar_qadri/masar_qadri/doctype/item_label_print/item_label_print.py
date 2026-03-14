288f75048b9a77d4ee121c1604255eaa00f2e642
karamakcsc <karamakcsc@hotmail.com>
2026-03-14 18:34:06 +0800

    update requerments

--- masar_qadri/masar_qadri/doctype/item_label_print/item_label_print.py
+++ masar_qadri/masar_qadri/doctype/item_label_print/item_label_print.py
@@ -1,115 +1,267 @@
+# # Copyright (c) 2025, KCSC and contributors
+# # For license information, please see license.txt
+
+# import frappe
+# import base64
+# from io import BytesIO
+# import barcode
+# from barcode.writer import ImageWriter
+# from frappe.model.document import Document
+
+
+# class ItemLabelPrint(Document):
+# 	def validate(self):
+# 		generate_barcode_py(self)
+		
+# 	@frappe.whitelist()
+# 	def fetch_items(self):
+# 		filters = {"disabled": 0, "has_variants": 0}
+
+# 		if self.item_code:
+# 			filters["item_code"] = self.item_code
+# 		if self.item_group:
+# 			filters["item_group"] = self.item_group
+# 		if self.description:
+# 			filters["custom_description_code"] = self.description
+
+# 		items = frappe.get_all(
+# 			"Item",
+# 			filters=filters,
+# 			fields=[
+# 				"item_code",
+# 				"item_name",
+# 				"item_group",
+# 				"brand",
+# 				"custom_description_code",
+# 				"name as item_name_id",
+# 			]
+# 		)
+
+# 		for item in items:
+# 			barcodes = frappe.get_all(
+# 				"Item Barcode",
+# 				filters={"parent": item.item_code},
+# 				fields=["barcode"]
+# 			)
+
+# 			price = frappe.db.get_value(
+# 				"Item Price",
+# 				{"item_code": item.item_code, "selling": 1},
+# 				"price_list_rate"
+# 			)
+
+# 			attributes = frappe.get_all(
+# 				"Item Variant Attribute",
+# 				filters={"parent": item.item_code},
+# 				fields=["attribute", "attribute_value"]
+# 			)
+# 			attr_map = {a.attribute: a.attribute_value for a in attributes}
+
+# 			if self.barcode and not any(b["barcode"] == self.barcode for b in barcodes):
+# 				continue
+# 			if self.article and attr_map.get("Article") != self.article:
+# 				continue
+# 			if self.size and attr_map.get("Size") != self.size:
+# 				continue
+# 			if self.style and attr_map.get("Style") != self.style:
+# 				continue
+# 			if self.season and attr_map.get("Season") != self.season:
+# 				continue
+# 			if self.color and attr_map.get("Color") != self.color:
+# 				continue
+
+# 			self.append("items", {
+# 				"item_code": item.item_code,
+# 				"item_name": item.item_name,
+# 				"barcode": barcodes[0]["barcode"] if barcodes else None,
+# 				"description": item.custom_description_code,
+# 				"item_group": item.item_group,
+# 				"brand": item.brand,
+# 				"price": price,
+# 				"size": attr_map.get("Size"),
+# 				"style": attr_map.get("Style"),
+# 				"season": attr_map.get("Season"),
+# 				"article": attr_map.get("Article"),
+# 				"color": attr_map.get("Color"),
+# 			})
+
+# def get_barcode(data, barcode_type):
+# 	barcode_class = barcode.get_barcode_class(barcode_type)
+# 	bar = barcode_class(data, writer=ImageWriter())
+
+# 	buffer = BytesIO()
+# 	bar.write(buffer, {
+# 		"module_width": 0.4,
+# 		"module_height": 16,
+# 		"quiet_zone": 1.5,
+# 		"font_size": 0,
+# 		"text_distance": 0,
+# 		"background": "white",
+# 		"foreground": "black",
+# 		"write_text": False,
+# 		"dpi": 900,
+# 	})
+	
+# 	base64_string = base64.b64encode(buffer.getvalue()).decode("utf-8")
+# 	return f"data:image/png;base64,{base64_string}"
+
+
+# @frappe.whitelist()
+# def generate_barcode_py(self):
+# 	if self.items:
+# 		for item in self.items:
+# 			if item.barcode:
+# 				bar = get_barcode(item.barcode, "code128")
+# 				item.barcode_text = bar
+
+
 # Copyright (c) 2025, KCSC and contributors
 # For license information, please see license.txt
 
 import frappe
 import base64
 from io import BytesIO
-import barcode
-from barcode.writer import ImageWriter
 from frappe.model.document import Document
+
+# Safe import to prevent crash if dependency missing
+try:
+    import barcode
+    from barcode.writer import ImageWriter
+except ImportError:
+    barcode = None
+    ImageWriter = None
 
 
 class ItemLabelPrint(Document):
-	def validate(self):
-		generate_barcode_py(self)
-		
-	@frappe.whitelist()
-	def fetch_items(self):
-		filters = {"disabled": 0, "has_variants": 0}
 
-		if self.item_code:
-			filters["item_code"] = self.item_code
-		if self.item_group:
-			filters["item_group"] = self.item_group
-		if self.description:
-			filters["custom_description_code"] = self.description
+    def validate(self):
+        generate_barcode_py(self)
 
-		items = frappe.get_all(
-			"Item",
-			filters=filters,
-			fields=[
-				"item_code",
-				"item_name",
-				"item_group",
-				"brand",
-				"custom_description_code",
-				"name as item_name_id",
-			]
-		)
+    @frappe.whitelist()
+    def fetch_items(self):
 
-		for item in items:
-			barcodes = frappe.get_all(
-				"Item Barcode",
-				filters={"parent": item.item_code},
-				fields=["barcode"]
-			)
+        filters = {"disabled": 0, "has_variants": 0}
 
-			price = frappe.db.get_value(
-				"Item Price",
-				{"item_code": item.item_code, "selling": 1},
-				"price_list_rate"
-			)
+        if self.item_code:
+            filters["item_code"] = self.item_code
 
-			attributes = frappe.get_all(
-				"Item Variant Attribute",
-				filters={"parent": item.item_code},
-				fields=["attribute", "attribute_value"]
-			)
-			attr_map = {a.attribute: a.attribute_value for a in attributes}
+        if self.item_group:
+            filters["item_group"] = self.item_group
 
-			if self.barcode and not any(b["barcode"] == self.barcode for b in barcodes):
-				continue
-			if self.article and attr_map.get("Article") != self.article:
-				continue
-			if self.size and attr_map.get("Size") != self.size:
-				continue
-			if self.style and attr_map.get("Style") != self.style:
-				continue
-			if self.season and attr_map.get("Season") != self.season:
-				continue
-			if self.color and attr_map.get("Color") != self.color:
-				continue
+        if self.description:
+            filters["custom_description_code"] = self.description
 
-			self.append("items", {
-				"item_code": item.item_code,
-				"item_name": item.item_name,
-				"barcode": barcodes[0]["barcode"] if barcodes else None,
-				"description": item.custom_description_code,
-				"item_group": item.item_group,
-				"brand": item.brand,
-				"price": price,
-				"size": attr_map.get("Size"),
-				"style": attr_map.get("Style"),
-				"season": attr_map.get("Season"),
-				"article": attr_map.get("Article"),
-				"color": attr_map.get("Color"),
-			})
+        items = frappe.get_all(
+            "Item",
+            filters=filters,
+            fields=[
+                "item_code",
+                "item_name",
+                "item_group",
+                "brand",
+                "custom_description_code",
+                "name as item_name_id",
+            ],
+        )
 
-def get_barcode(data, barcode_type):
-	barcode_class = barcode.get_barcode_class(barcode_type)
-	bar = barcode_class(data, writer=ImageWriter())
+        for item in items:
 
-	buffer = BytesIO()
-	bar.write(buffer, {
-		"module_width": 0.4,
-		"module_height": 16,
-		"quiet_zone": 1.5,
-		"font_size": 0,
-		"text_distance": 0,
-		"background": "white",
-		"foreground": "black",
-		"write_text": False,
-		"dpi": 900,
-	})
-	
-	base64_string = base64.b64encode(buffer.getvalue()).decode("utf-8")
-	return f"data:image/png;base64,{base64_string}"
+            barcodes = frappe.get_all(
+                "Item Barcode",
+                filters={"parent": item.item_code},
+                fields=["barcode"]
+            )
+
+            price = frappe.db.get_value(
+                "Item Price",
+                {"item_code": item.item_code, "selling": 1},
+                "price_list_rate"
+            )
+
+            attributes = frappe.get_all(
+                "Item Variant Attribute",
+                filters={"parent": item.item_code},
+                fields=["attribute", "attribute_value"]
+            )
+
+            attr_map = {a.attribute: a.attribute_value for a in attributes}
+
+            if self.barcode and not any(b["barcode"] == self.barcode for b in barcodes):
+                continue
+
+            if self.article and attr_map.get("Article") != self.article:
+                continue
+
+            if self.size and attr_map.get("Size") != self.size:
+                continue
+
+            if self.style and attr_map.get("Style") != self.style:
+                continue
+
+            if self.season and attr_map.get("Season") != self.season:
+                continue
+
+            if self.color and attr_map.get("Color") != self.color:
+                continue
+
+            self.append("items", {
+                "item_code": item.item_code,
+                "item_name": item.item_name,
+                "barcode": barcodes[0]["barcode"] if barcodes else None,
+                "description": item.custom_description_code,
+                "item_group": item.item_group,
+                "brand": item.brand,
+                "price": price,
+                "size": attr_map.get("Size"),
+                "style": attr_map.get("Style"),
+                "season": attr_map.get("Season"),
+                "article": attr_map.get("Article"),
+                "color": attr_map.get("Color"),
+            })
+
+
+def get_barcode(data, barcode_type="code128"):
+
+    if not barcode:
+        return None
+
+    barcode_class = barcode.get_barcode_class(barcode_type)
+
+    bar = barcode_class(data, writer=ImageWriter())
+
+    buffer = BytesIO()
+
+    bar.write(buffer, {
+        "module_width": 0.4,
+        "module_height": 16,
+        "quiet_zone": 1.5,
+        "font_size": 0,
+        "text_distance": 0,
+        "background": "white",
+        "foreground": "black",
+        "write_text": False,
+        "dpi": 900,
+    })
+
+    base64_string = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+    return f"data:image/png;base64,{base64_string}"
 
 
 @frappe.whitelist()
-def generate_barcode_py(self):
-	if self.items:
-		for item in self.items:
-			if item.barcode:
-				bar = get_barcode(item.barcode, "code128")
-				item.barcode_text = bar
+def generate_barcode_py(doc):
+
+    if not barcode:
+        frappe.msgprint("Python barcode library is not installed")
+        return
+
+    if doc.items:
+
+        for item in doc.items:
+
+            if item.barcode:
+
+                barcode_image = get_barcode(item.barcode, "code128")
+
+                if barcode_image:
+                    item.barcode_text = barcode_image
